a34ff75b2d9b58ecb80e31f85a4ebeb3500bd77f
Diego Hernandez <diegoalejandro061292@gmail.com>
2023-02-01 19:06:30 -0500

    codigo cajero

---
+++ script.js
@@ -0,0 +1,23 @@
+const nombre =prompt ("seleccione su cuenta");
+let contraseña = prompt('ingrese su contraseña')
+
+const cuentas=[
+    { nombre: 'Mali', saldo: 200, contraseña1: 1 },
+    { nombre: 'Gera', saldo: 290, contraseña2: 2 },
+    { nombre: 'Maui', saldo: 67,  contraseña3: 3 }
+];
+// if (nombre==='mali' || nombre==='gera' || nombre==='maui'){console.log("datos correctos")}
+if ( nombre=='mali' && contraseña==1 || nombre=='gera'&& contraseña==2 || nombre=='maui' && contraseña==3){console.log("datos correctos")}
+else if ( contraseña !== nombre){console.log("contraseña incorrecta")}
+else if (){console.log('p')}
+
+
+function usuario(nombre){
+for (let i = 0; i < users.length; i++) {
+    if (users[i].nombre === nombre) {
+      return i;
+    }
+}
+return -1;
+}
+console.log(nombre);
